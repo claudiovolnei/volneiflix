f0917877e08273bfd0834e50698bdbeb8f840217
Claudio Volnei da Silva Junior <claudio.silva@ipmmi.local>
2020-07-28 13:47:50 -0300

    Criando rotas no app

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/Video';
+
+// Desafio master blaster na descrição
+const Pagina404 = () => (<div>Página 404</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={App} exact />    
+      <Route path="/cadastro/video" component={CadastroVideo} />    
+      <Route component={Pagina404} />   
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
